ce7c129c60e38f6a4a2e7cd5e99d69185f4e8f59
Emmanuel Ejei-Okeke <ejeiokekeemmanuel@gmail.com>
2025-02-21 13:56:10 +0100

    feat: add main background on communities page

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -3,7 +3,11 @@
 import JoinCommunity from "@/app/components/join-community";
 
 const CommunityPage = () => {
-  return <JoinCommunity />;
+  return (
+    <section className="grid place-items-center h-screen bg-main-bg bg-cover">
+      <JoinCommunity />
+    </section>
+  );
 };
 
 export default CommunityPage;
