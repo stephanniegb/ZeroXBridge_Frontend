74934402b2e718448c1b810896ff10ca3b71f73e
Michelle031 <mbaangel2803@gmail.com>
2025-03-21 16:45:06 +0100

    Feat: Implement Light mode for all Success state components

--- app/Sidebar/page.tsx
+++ app/Sidebar/page.tsx
@@ -2,9 +2,12 @@
 import { useState } from "react";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/Sidebar";
+import AnimatedIcon from "../components/success-state/animated-icon";
+import SuccessState from "../components/success-state/success-state";
 
 export default function Home() {
     const [isDarkMode, setIsDarkMode] = useState(true);
+    const [open, setOpen] = useState(true)
 
     return (
         <div className={`${isDarkMode ? "bg-[#09050e]" : "bg-white"} h-screen flex flex-col`}>
@@ -13,6 +16,17 @@ export default function Home() {
                 <Sidebar isDarkMode={isDarkMode} />
                 <main className="flex-1">
                     {/* Main content goes here */}
+                    <SuccessState 
+                        isOpen={open} 
+                        onClose={setOpen}
+                        message={'You have successfully LOCKED $200 xZB Tokens.'}
+                        isDarkMode={isDarkMode}
+                        icon={<AnimatedIcon iconType='lock' />}
+                    />
+                    {/* this button is used to test the light and dark by opening the success modal since, the navbar is displayed in this component as in the figma file */}
+                    <button onClick={() => setOpen(!open)} className={`${isDarkMode ? "text-white ":"text-[#09050e]"} text-xl p-4 rounded-md text-white`}>
+                        Click to open success state
+                    </button>
                 </main>
             </div>
         </div>
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -5,13 +5,16 @@ import AnimatedIcon from "../components/success-state/animated-icon";
 
 export default function index (){
     const [open, setOpen] = useState(true)
+    const [isDarkMode, setIsDarkMode] = useState(true);
     return(
+        
         <div className="bg-main-bg bg-no-repeat bg-cover">
             <SuccessState 
             isOpen={open} 
             onClose={setOpen}
-            message={'You have successfully SWAPPED 0.00034 ETH to 5.79 SOL'}
-            icon={<AnimatedIcon iconType='swap' />}
+            message={'You have successfully LOCKED $200 xZB Tokens.'}
+            isDarkMode={isDarkMode}
+            icon={<AnimatedIcon iconType='lock' />}
             />
         </div>
     );
