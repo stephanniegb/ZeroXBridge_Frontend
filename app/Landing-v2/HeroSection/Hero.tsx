cf0e5b5331cc2ef345280616e3bf1bc2975bd10b
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-07-24 20:56:08 +0100

    Update Hero.tsx

--- app/Landing-v2/HeroSection/Hero.tsx
+++ app/Landing-v2/HeroSection/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           </h1>
           <p className="">
             Unlock liquidity on Starknet using ZK,{" "}
-            <span className="text-[#6C6C6C] ">no centralized bridges.</span>
+            <span className="text-[#6C6C6C]">no centralized bridges.</span>
           </p>
           <div className="flex gap-2 items-center ">
             <button className="bg-white  rounded-full text-black px-4 py-2 flex items-center gap-2">
