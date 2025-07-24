ed894d2cba8b8d6c529bbc7346811caf9234b8d2
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-07-24 20:56:50 +0100

    Update page.tsx

--- app/lock-summary/page.tsx
+++ app/lock-summary/page.tsx
@@ -2,7 +2,7 @@ import LockSummary from "../components/lock-summary";
 
 export default function Page() {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-white">
+    <div className="w-full h-screen  flex items-center justify-center bg-white">
       <button>open Modal</button>
       <LockSummary />
     </div>
