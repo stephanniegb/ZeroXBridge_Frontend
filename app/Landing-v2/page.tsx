49fcbbe31b11c36cf5a17527bcb44274afb59d86
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-07-26 14:57:17 +0100

    Update page.tsx

--- app/Landing-v2/page.tsx
+++ app/Landing-v2/page.tsx
@@ -2,12 +2,20 @@
 
 import { InsideZeroX } from "./InsideZeroX";
 import { FeaturesList } from "./ComingSoonFeatures";
+import { HowItWorks } from "./HowItWorks";
+import { ZeroXPosition } from "./Position";
+import { Hero } from "./HeroSection";
 
 const Page = () => {
   return (
-    <div className="w-full min-h-screen h-full bg-[#0a0a0a] px-4 py-8 flex flex-col items-center ">
-      <InsideZeroX />
-      <FeaturesList />
+    <div className="w-full min-h-screen h-full bg-[#0a0a0a]  flex flex-col items-center">
+      <Hero />
+      <div className="bg-[#0A0A0A] w-full relative z-10">
+        <ZeroXPosition />
+        <HowItWorks />
+        <InsideZeroX />
+        <FeaturesList />
+      </div>
     </div>
   );
 };
